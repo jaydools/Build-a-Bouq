a45f227b839b636f7726764e7183b7bf37110f6b
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-14 10:01:04 -0500

    first commit on new branch
    began setting up nav bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./Header.scss";
 
 function Header() {
-  return <div>Header</div>;
+    return (
+        <nav className="navbar">
+            <div className="leftside">
+                <p className="leftside__logo">Logo</p>
+            </div>
+            <div className="rightside">
+                <ul className="rightside__list-parent">
+                    <li className="rightside__list-item">Cart</li> {/* "Link to" this part */}
+                    <li className="rightside__list-item">About Us</li>
+                    <li className="rightside__list-item">Login</li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Header;
