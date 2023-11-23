fdf0a10584fa59383812e21cc985b9282af86e3e
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-22 19:07:03 -0500

    started staying down into builder
    need to fix styling for mobile

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Header.scss";
 import { Link } from "react-router-dom";
+import logo from "../../Assets/Images/logo.svg";
+import cartStatic from "../../Assets/Images/cart-static.png";
 
 function Header() {
     return (
         <nav className="navbar">
             <div className="leftside">
-                <p className="leftside__logo">Logo</p>
+                <p className="leftside__logo">
+                    <img src={logo} alt="logo" className="logo" />
+                </p>
             </div>
             <div className="rightside">
                 <ul className="rightside__list-parent">
@@ -14,13 +18,13 @@ function Header() {
                         <Link to="/">Home</Link>
                     </li>
                     <li className="rightside__list-item">
-                        <Link to="/cart">Cart</Link>
-                    </li>
-                    <li className="rightside__list-item">
                         <Link to="/about-us">About Us</Link>
                     </li>
                     <li className="rightside__list-item">
-                        <Link to="/login">Login</Link>
+                        <Link to="/cart">
+                            Cart
+                            <img src={cartStatic} alt="shopping cart" className="cart-static" />
+                        </Link>
                     </li>
                 </ul>
             </div>
