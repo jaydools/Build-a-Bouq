9d00f22ccd5893358e3a54a7ed9d29b49000d386
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-27 10:18:49 -0500

    made the container element have the highest Z index for previewer

--- src/Components/Previewer/Previewer.js
+++ src/Components/Previewer/Previewer.js
@@ -16,22 +16,31 @@ function Previewer() {
 
     return (
         <div className="image-container">
-            {allImages.length > 0 ? (
-                allImages.map((url, index) => (
-                    <div
-                        key={index}
-                        className="image-container__container"
-                        style={{ zIndex: zIndexRando() }}
-                    >
-                        <img
-                            className="image-container__image"
-                            src={url}
-                            alt={`Preview ${index}`}
-                        />
-                    </div>
-                ))
-            ) : (
-                // Render a placeholder image if allImages is empty
+            {/* Iterating over each category and its images */}
+            {Object.entries(images).map(([category, imageUrls]) =>
+                imageUrls.map(
+                    (
+                        // Map each image URL to an image element
+                        url,
+                        index
+                    ) => (
+                        <div
+                            key={`${category}-${index}`} // Unique key for React list rendering
+                            className="image-container__container"
+                            // Conditional styling: if category is 'container', set z-index to 1000, otherwise use random z-index
+                            style={{ zIndex: category === "container" ? 1000 : zIndexRando() }}
+                        >
+                            <img
+                                className="image-container__image"
+                                src={url}
+                                alt={`Preview ${index}`}
+                            />
+                        </div>
+                    )
+                )
+            )}
+            {/* Fallback content: if there are no images, display a placeholder image */}
+            {Object.keys(images).every((category) => images[category].length === 0) && (
                 <div className="image-container__container">
                     <img className="image-container__image" src={placeholder} alt="Placeholder" />
                 </div>
